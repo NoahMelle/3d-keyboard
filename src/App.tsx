d1e7376615f49de6f25549d8fafbb30956e4e606
NoahMelle <noahmkamphuisen@gmail.com>
2025-02-01 19:46:16 +0100

    Added audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
 import Scene from "./Scene";
-import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import {
     Environment,
     CameraControls,
     PerspectiveCamera,
 } from "@react-three/drei";
+import { useRef, forwardRef } from "react";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 function App() {
+    const planeRef = useRef<THREE.Mesh | null>(null);
+    const cameraControlsRef = useRef<CameraControls>(null);
+
+    useFrame(() => {
+        if (
+            planeRef.current &&
+            cameraControlsRef.current?.colliderMeshes.length === 0
+        ) {
+            cameraControlsRef.current.colliderMeshes = [planeRef.current];
+        }
+    });
+
     return (
-        <Canvas>
-            <Environment
-                files="/assets/hdris/lebombo_1k.exr"
-                environmentIntensity={0.8}
-            />
+        <>
+            <Environment environmentIntensity={0.8} preset="apartment" />
+            <Plane args={[4, 4]} ref={planeRef} />
+            <spotLight castShadow position={[0, 10, 0]} />
             <ambientLight intensity={0.5} />
-            <PerspectiveCamera makeDefault position={[.6, .6, 1]} />
+            <PerspectiveCamera
+                makeDefault
+                position={[0.6, 0.6, 1]}
+                near={0.1}
+                far={1000}
+            />
             <CameraControls
-                mouseButtons={{ left: 0, middle: 1, right: 2, wheel: 0 }}
+                mouseButtons={{ left: 0, middle: 1, right: 0, wheel: 0 }}
+                ref={cameraControlsRef}
             />
             <Scene />
-        </Canvas>
+        </>
     );
 }
 
 export default App;
+
+const Plane = forwardRef<THREE.Mesh, { args: [number, number] }>(
+    ({ args, ...props }, ref) => {
+        return (
+            <mesh
+                rotation={[-Math.PI / 2, 0, 0]}
+                position={[0, -0.6, 0]}
+                receiveShadow
+                ref={ref}
+                {...props}
+            >
+                <planeGeometry args={args} />
+                <shadowMaterial transparent opacity={0.1} />
+            </mesh>
+        );
+    }
+);
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,13 +1,10 @@
 import { Keyboard } from "./components/models/Keyboard";
 
 export default function Scene() {
-    return <Box />;
-}
-
-const Box = () => {
     return (
         <>
             <Keyboard />
+
         </>
     );
-};
+}
